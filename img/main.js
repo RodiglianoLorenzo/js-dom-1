830032157d00d0a572df5b8b1d90676e01ad1403
Lorenzo Rodigliano <rodiglianolorenzo@gmail.com>
2026-02-11 14:17:50 +0100

    compelte exercise

--- img/main.js
+++ img/main.js
@@ -1,3 +1,19 @@
+const imageEl = document.querySelector("img")
+console.log(imageEl);
+
+const buttonEl = document.querySelector("button")
+console.log(buttonEl);
+
+function buttonClick() {
+    console.log("You click the button:");
+    
+}
+
+buttonEl.onclick = function() {
+    imageEl.src = "yellow_lamp.png"
+    buttonEl.innerHTML = "spegni"
+}
+
 
 
 
